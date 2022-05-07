5167425278c5b54f4d2b7c8c28b434c223edd9c8
Flo Youtube 54 <f.goussot@outlook.com>
2022-05-07 15:27:17 +0200

    Ajout de l'interface

--- NoScreenCDPlayer/Form1.Designer.vb
+++ NoScreenCDPlayer/Form1.Designer.vb
@@ -23,29 +23,218 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.FichierToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OuvrirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PériphériqueToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DossierToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.QuitterToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OptionsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OuvrirEtJouerLorsquunPériphériqueEstInséréToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'MenuStrip1
         '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FichierToolStripMenuItem, Me.OptionsToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
         Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(660, 390)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(128, 48)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Piste suivante >"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 390)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(128, 48)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "< Piste précédente"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(147, 390)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(128, 48)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Pause"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(526, 390)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(128, 48)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Lecture"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(340, 390)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(128, 48)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "Arrêt"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Cursor = System.Windows.Forms.Cursors.SizeWE
+        Me.TrackBar1.Location = New System.Drawing.Point(12, 327)
+        Me.TrackBar1.Maximum = 100
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(776, 45)
+        Me.TrackBar1.TabIndex = 6
+        Me.TrackBar1.TickFrequency = 0
+        Me.TrackBar1.TickStyle = System.Windows.Forms.TickStyle.Both
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4})
+        Me.ListView1.HideSelection = False
+        Me.ListView1.Location = New System.Drawing.Point(12, 27)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(776, 294)
+        Me.ListView1.TabIndex = 7
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "#"
+        Me.ColumnHeader1.Width = 32
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Titre"
+        Me.ColumnHeader2.Width = 256
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Artiste"
+        Me.ColumnHeader3.Width = 256
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Album"
+        Me.ColumnHeader4.Width = 128
+        '
+        'FichierToolStripMenuItem
+        '
+        Me.FichierToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OuvrirToolStripMenuItem, Me.ToolStripMenuItem1, Me.QuitterToolStripMenuItem})
+        Me.FichierToolStripMenuItem.Name = "FichierToolStripMenuItem"
+        Me.FichierToolStripMenuItem.Size = New System.Drawing.Size(54, 20)
+        Me.FichierToolStripMenuItem.Text = "&Fichier"
+        '
+        'OuvrirToolStripMenuItem
+        '
+        Me.OuvrirToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PériphériqueToolStripMenuItem, Me.DossierToolStripMenuItem})
+        Me.OuvrirToolStripMenuItem.Name = "OuvrirToolStripMenuItem"
+        Me.OuvrirToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.OuvrirToolStripMenuItem.Text = "&Ouvrir"
+        '
+        'PériphériqueToolStripMenuItem
+        '
+        Me.PériphériqueToolStripMenuItem.Name = "PériphériqueToolStripMenuItem"
+        Me.PériphériqueToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.PériphériqueToolStripMenuItem.Text = "&Périphérique"
+        '
+        'DossierToolStripMenuItem
+        '
+        Me.DossierToolStripMenuItem.Name = "DossierToolStripMenuItem"
+        Me.DossierToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.DossierToolStripMenuItem.Text = "&Dossier"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(177, 6)
+        '
+        'QuitterToolStripMenuItem
+        '
+        Me.QuitterToolStripMenuItem.Name = "QuitterToolStripMenuItem"
+        Me.QuitterToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.QuitterToolStripMenuItem.Text = "&Quitter"
+        '
+        'OptionsToolStripMenuItem
+        '
+        Me.OptionsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OuvrirEtJouerLorsquunPériphériqueEstInséréToolStripMenuItem})
+        Me.OptionsToolStripMenuItem.Name = "OptionsToolStripMenuItem"
+        Me.OptionsToolStripMenuItem.Size = New System.Drawing.Size(61, 20)
+        Me.OptionsToolStripMenuItem.Text = "&Options"
+        '
+        'OuvrirEtJouerLorsquunPériphériqueEstInséréToolStripMenuItem
+        '
+        Me.OuvrirEtJouerLorsquunPériphériqueEstInséréToolStripMenuItem.CheckOnClick = True
+        Me.OuvrirEtJouerLorsquunPériphériqueEstInséréToolStripMenuItem.Name = "OuvrirEtJouerLorsquunPériphériqueEstInséréToolStripMenuItem"
+        Me.OuvrirEtJouerLorsquunPériphériqueEstInséréToolStripMenuItem.Size = New System.Drawing.Size(325, 22)
+        Me.OuvrirEtJouerLorsquunPériphériqueEstInséréToolStripMenuItem.Text = "&Ouvrir et jouer lorsqu'un périphérique est inséré"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents FichierToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OuvrirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PériphériqueToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DossierToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As ToolStripSeparator
+    Friend WithEvents QuitterToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OptionsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OuvrirEtJouerLorsquunPériphériqueEstInséréToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents ColumnHeader3 As ColumnHeader
+    Friend WithEvents ColumnHeader4 As ColumnHeader
 End Class
